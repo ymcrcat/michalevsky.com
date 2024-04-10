7e9bdab40d9eea40f95c7624b8edb34e3012df76
Yan Michalevsky <yan@cryptosat.io>
2024-04-10 17:21:47 -0400

    Updated CSS

--- src/components/MainLeft.js
+++ src/components/MainLeft.js
@@ -3,7 +3,9 @@ import photo from '../assets/yan.png';
 function MainLeft(props) {
     return ( 
         <section id='mainLeft'>
-            <img src={photo} alt = {props.authorName} width='150' />
+            <section className='columns'>
+                <img src={photo} alt = {props.authorName} width='150' />
+            </section>
         </section> 
     );
 }
