d1256bea6cbda08ef3ee93ad20d8cc9079dd53a7
Yan Michalevsky <ymcrcat@gmail.com>
2025-05-06 14:34:04 -0400

    Removed old music

--- src/components/Music.js
+++ src/components/Music.js
@@ -22,7 +22,7 @@ function Music(props) {
                     src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/44197455&amp;color=ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_artwork=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false"/>
             </section>
 
-            <section className="columns">
+            {/* <section className="columns">
                 <h3>Old</h3>
                 This section contains some music I have composed long ago. Several pieces were
                 written together with Vitaly Koshelev, using various DAWs software was used including
@@ -32,9 +32,9 @@ function Music(props) {
                 , <a href="http://www.rebirthmuseum.com/">Rebirth</a>
                 , and <a href="http://www.propellerheads.se/products/recycle/">Recycle</a>.
                 Composed during the period of 1997-2000.
-            </section>
+            </section> */}
 
-            <section className="columns">
+            {/* <section className="columns">
                 <a href="http://www.michalevsky.com/music/lovefire_rmx.mp3">Lovefire (House Remix)</a> - produced using ModPlug Tracker.<br/>
                 <a href="http://www.michalevsky.com/music/wam.mp3">Walk Around Mad (House Remix)</a> - Yan Michalevsky and Vitaly Koshelev (V-Shark).<br/>
                 <a href="http://www.michalevsky.com/music/rnarnd.mp3">Round and Round</a> - A remix of the famous dance track by Ti.Pi.Cal.
@@ -43,7 +43,7 @@ function Music(props) {
                 <a href="http://www.michalevsky.com/music/Afterdark1.mp3">After-dark Echoes</a> - produced using ModPlug Tracker.<br/>
                 <a href="http://www.michalevsky.com/music/CyberSound-Track4.mp3">Track 4</a> - Unnamed track from CyberSound album.
                 Composed using MicroLogic.<br/>
-            </section>
+            </section> */}
         </div>
     )
 }
