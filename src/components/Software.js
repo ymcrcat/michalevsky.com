50e181a238739c34487f61ef8f6553104916a51f
Yan Michalevsky <ymcrcat@gmail.com>
2025-10-29 15:21:20 -0400

    Make software titles bold

--- src/components/Software.js
+++ src/components/Software.js
@@ -2,7 +2,7 @@ function Software(props) {
     return (
         <section className="columns content-card">
 
-            <p><h3><a href="http://www.boxstreamapp.com">BoxStream</a></h3>
+            <p><h3><a href="http://www.boxstreamapp.com"><b>BoxStream</b></a></h3>
             BoxStream is a cloud music player for Android, currently supporting streaming music from Dropbox.
             Dropbox is a great way to store your music or podcasts and organize them conveniently,
             so that they are accessible on each one of your devices.
@@ -10,7 +10,7 @@ function Software(props) {
             the <a href="https://play.google.com/store/apps/details?id=com.michalevsky.boxstream">Google Play Store</a>.
             </p>
 
-            <p><h3><a href="http://faust.grame.fr/">Faust</a></h3>
+            <p><h3><a href="http://faust.grame.fr/"><b>Faust</b></a></h3>
             I used to contribute to the development of the VSTi architecture for the Faust
             musical signal processing language.<br/>
             Faust currently supports VST audio effects and instruments for 32 and 64-bit architectures.
