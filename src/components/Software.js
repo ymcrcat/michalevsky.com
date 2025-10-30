13ca4cff63ba72bc27e2c24e032e165153301e58
Yan Michalevsky <ymcrcat@gmail.com>
2025-10-30 19:30:36 -0400

    Updated software section

--- src/components/Software.js
+++ src/components/Software.js
@@ -2,17 +2,21 @@ function Software(props) {
     return (
         <section className="columns content-card">
 
-            <p><h3><a href="http://www.boxstreamapp.com"><b>BoxStream</b></a></h3>
-            BoxStream is a cloud music player for Android, currently supporting streaming music from Dropbox.
+            <p><h3><a href="https://github.com/ymcrcat/soroban-privacy-pools"><b>Privacy Pools for Stellar</b></a></h3>
+            A prototype of Privacy Pools for the <a href="https://stellar.org">Stellar</a> blockchain.
+            </p>
+            <br/>
+            <p><h3><b>BoxStream</b></h3>
+            BoxStream is a cloud music player for Android, currently supporting streaming audio from Dropbox.
             Dropbox is a great way to store your music or podcasts and organize them conveniently,
             so that they are accessible on each one of your devices.
-            The application is available for download from 
+            The application is available on 
             the <a href="https://play.google.com/store/apps/details?id=com.michalevsky.boxstream">Google Play Store</a>.
             </p>
-
+            <br/>
             <p><h3><a href="http://faust.grame.fr/"><b>Faust</b></a></h3>
             I used to contribute to the development of the VSTi architecture for the Faust
-            musical signal processing language.<br/>
+            musical signal processing language.
             Faust currently supports VST audio effects and instruments for 32 and 64-bit architectures.
             </p>
 
